c00c9eb4c820dcf510e0814654ddc292d4eeb43d
Laszlo Forro <getforum@gmail.com>
2013-06-03 07:28:30 +0200

    In the flow

--- src/Elements/Sequences.py
+++ src/Elements/Sequences.py
@@ -1,59 +1,76 @@
 from math import log
 import Token
 
+class Sequences(object):
+    """Sequences are an other concept of the universe. These are the visible phenomenae, the product of 
+    engines applying relations. The original input - for example text - is the starting sequence, but 
+    any graph can be a sequence. These are not entities, but end-products or sources,
+    a combination of different elements of rules and relations.
+    Every sequence must know iteritems() in the base class.
+    Init parameters: 
+        - tokens instance
+        Bool:
+        - increment_co-occurrence (True) : automatically increment the co-occurrences as a sequence is given.
+            Otherwise self.add_co_occurrences must be called.
+        - drop_duplicates (True) : if a sequence appeares multiple times in a text unit, it is dropped
+        - head_token (False) : the abstract head token is counted or not
+    """
 
-# can it be a  Builder class?
+    def __init__(self, tokens, increment_co-occurrence=True, drop_duplicates=True, head_token=False):
+        self._sequence = {}
+        self.active = ''
+        self.tokens = tokens
+        self.increment_co-occurrence = increment_co-occurrence
+        self.drop_duplicates = drop_duplicates
+        self.head_token=head_token
+        if head_token is True:
+            tokens.insert(0, '_head_')
 
-def add_sequence_of_tokens(sequences, title, tokenlist):
-        """Convenience method to process a sequence into the text and token containers."""
 
-        sequences.add_text(title)
-        tokenlist.insert(0, '_head_')
-        tokens = self.tokens.add_tokenlist(tokenlist)
+    def add_sequence_of_tokens(self,sequence):
+        """Processes the sequence of tokens into the instance sequence container"""
 
-        sequences.add_token_ids([ t.idx for t in tokens])
+        sequences.add_unit(title)
+        sequences.add_token_ids([ t.idx for t in sequence])
+        if self.increment_co-occurrence:
+            add_co_occurrence(sequence)
 
-def add_co_occurrences(sequences, tokens):
+
+    def add_co_occurrences(self,sequence=None):
         """Adds the co-occurrence of two words. The co_occurrences attribute is the data for joined computations.
         It is important that co-occurrence may mean different things. At the moment we think that two elements 
         appeare in the same sequence."""
 
-        tokens = token.tokens
-
+        if not sequence:
+            sequence = self.sequences.text.values()
         # bug? : in case : 'a a' co_occurrence is : 2, not 1 !!!!!
-        for sequences in sequences.text.values():  # get the list of sentences
+        for sequences in self.sequences.text.values():  # get the list of sentences
 
             for sen in sequences:  # iterate over the sentences
+                self._add_co-occurrence(sen)
              #   print (sen)
-                for c in range(1, len(sen)):  # skip the head token   # XXX no auto vivification
+       
 
-                    token = tokens[ sen[c] ]
-                    for i in range(1, len(sen)):  # this is the all with all loop
-                        if c != i:
-                            if sen[i] in token.co_occurrence:
-                                token.co_occurrence[ sen[i] ] += 1
-                            else:
-                                token.co_occurrence[ sen[i] ] = 1
-            #            print (" %d : %d " % (c, i))
+    def _add_co-occurrence(self, sequence ):
 
+        tokens = self.token.tokens # TODO: interface will change
+        sequences = self.sequences
 
+        for c in range(0, len(sequence)): # XXX check head token case
+            token = tokens[ sen[c] ]
+            for i in range(1, len(sequence)):  # this is the all with all loop
+                if c != i:
+                    if sen[i] in token.co_occurrence:
+                        token.co_occurrence[ sequence[i] ] += 1
+                    else:
+                        token.co_occurrence[ sequence[i] ] = 1
+            # print (" %d : %d " % (c, i))
 
-class Sequences(object):
-    """Sequences are an other concept of the universe of communication. These are the visible phenomenae, the product of 
-    engines applying relations. The original input - for example text - is the starting sequence, but 
-    any graph can be a sequence. These are not entities, but end-products, a combination of different elements 
-    of rules and relations.
-    Every sequence must know iteritems() in the base class."""
-
-    def __init__(self, tokens):
-        self.sequence = {}
-        self.active = ''
-        self.tokens = tokens
-
-    def add_text(self, source):
-        """Start a new text unit."""
-
-        if source in self.text:
+    # TODO: sequences will get a list of sequences.
+    def add_unit(self, source):
+        """Start a new sequence unit."""
+        
+        if source in self.text and self.drop_duplicates is True:
             return self.text[source]
 
         self.sequence[source] = []
@@ -69,7 +86,7 @@ class Sequences(object):
         if title == []:
             title = self.sequence.keys()
             for t in title:  # this is each sentence list
-                yield (t, self.sequence[t])
+                yield (t, self.sequence[t]) 
 
 # these are copy-paste iterators but a. faster, b. I do not know the use cases
     def get_senquences_by_id(self):
@@ -91,3 +108,12 @@ class Sequences(object):
                 for title, i in self.sequence.iteritems():
                     for s in i:
                         yield title, [ tokens[id].name for id in s ]
+
+
+    @property
+    def sequence(self):
+        if self._sequence == 'pkl':
+            """ read in pkl. we should also set a handler to save it after a while..."""
+        
+        return self._sequence[active]
+
